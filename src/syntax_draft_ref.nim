fccfb9a22124a1e619a0acefa29007ab1b3c76a8
Franklin Marín <franklin.e.marin@gmail.com>
2026-07-16 01:31:52 -0300

    Progress in draft syntax doc.

--- src/syntax_draft_ref.nim
+++ src/syntax_draft_ref.nim
@@ -1,5 +1,8 @@
 #[ This is a multi line comment
     <comment>
+    #[
+      Nested comment.
+    ]#
 ]#
 
 import std/strformat
@@ -43,3 +46,32 @@ echo fmt"a={a}, b={b}, c={c}, d={d}, e={e}, f={f}, alphabet={alphabet}"
 # Compile-time error, const cannot be modified at run-time --> alphabet = "abc"
 
 # Compile-time error, `d` and `e` are immutable --> d.add("bar"), e += 1
+
+# Read input
+echo fmt"Insert your name: "
+var name: string = readLine(stdin)
+echo fmt"and your surname: "
+var surname: string = readLine(stdin)
+
+if name == "" and surname == "":
+  echo "Poor soul, you lost your name?"
+elif name == "name" or surname == "surname":
+  echo "Very funny, your name is name or your surname is surname."
+else:
+  echo fmt"Hello, {name} {surname}!"
+
+case name
+of "Dave", "Frank":
+  echo "Cool name!"
+else:
+  echo "Already greeted!"
+
+# raw literal
+echo "raw string -->", r" C:\program files\nim"
+
+var one_million = 1_000_000
+var float_point_literal = 1.0e9 # (one billion)
+var hexadecimal_literal = 0x1A # (26 in decimal)
+var binary_literal = 0b1010 # (10 in decimal)
+var octal_literal = 0o755 # (493 in decimal)
+echo fmt"one_million={one_million}, float_point_literal={float_point_literal}, hexadecimal_literal={hexadecimal_literal}, binary_literal={binary_literal}, octal_literal={octal_literal}"
